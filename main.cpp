ef9f7eea12012dbef927c51d81c3ab93e4383afe
Phoenix Ani <phoenixdafrog@gmail.com>
2026-03-27 00:23:56 +0000

    Window can close

--- main.cpp
+++ main.cpp
@@ -3,15 +3,23 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(1000,1600),"cool math games.com");
-    while (window.isOpen())
+    RenderWindow window(VideoMode(1600, 1000), "cool math games.com",
+                        Style::Default /*defaults to Resize, Titlebar and Close*/);
+    //window.setVerticalSyncEnabled(true); //VSync = matches monitor refresh rate
+    window.setFramerateLimit(20);
+
+    while (window.isOpen()) //main game loop
     {
         Event event;
-        while (window.pollEvent(event));
+        while (window.pollEvent(event)) //checks for everything that happened since the last frame
         {
-            //do nothing
+            if (event.type == Event::Closed /*if the close button is pressed*/) window.close();
         }
+
+        //logic handling
+        //rendering
         window.clear();
+        //draw things then...
         window.display();
     }
 
